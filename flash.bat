b9b89a7c162d956c951572c1b6f0eb13ead5eb7b
tarushi-singh <tarushi.singh@eulermotors.com>
2026-03-06 18:59:22 +0530

    flash changes

--- flash.bat
+++ flash.bat
@@ -1,48 +1,44 @@
 @echo off
 setlocal
 
-:: -------------------------------
-:: Select target to flash
-:: -------------------------------
 echo Select target to flash:
 echo 1. Bootloader
 echo 2. App Slot A
 echo 3. App Slot B
 set /p TARGET_CHOICE=Enter choice (1/2/3): 
 
-if "%TARGET_CHOICE%"=="1" set ELF_FILE=build\bootloader.elf
-if "%TARGET_CHOICE%"=="2" set ELF_FILE=build\app_slotA.elf
-if "%TARGET_CHOICE%"=="3" set ELF_FILE=build\app_slotB.elf
+if "%TARGET_CHOICE%"=="1" (
+    set FILE=build/bootloader.bin
+    set ADDR=0x08000000
+)
 
-:: -------------------------------
-:: Flashing
-:: -------------------------------
+if "%TARGET_CHOICE%"=="2" (
+    set FILE=build/app_slotA.bin
+    set ADDR=0x08008000
+)
+
+if "%TARGET_CHOICE%"=="3" (
+    set FILE=build/app_slotB.bin
+    set ADDR=0x08040000
+)
+
 echo ======================================
-echo Flashing %ELF_FILE%
+echo Flashing %FILE% at %ADDR%
 echo ======================================
 
-if not exist %ELF_FILE% (
-    echo ERROR: ELF file not found.
-    echo Please build the project first.
+if not exist %FILE% (
+    echo ERROR: File not found.
+    echo Build the project first.
     exit /b 1
 )
 
-openocd ^
--f interface/stlink.cfg ^
--f target/stm32f4x.cfg ^
--c "program %ELF_FILE% verify reset exit"
+openocd -f interface/stlink.cfg -f target/stm32f4x.cfg -c "program %FILE% %ADDR% verify reset exit"
 
 if errorlevel 1 goto error
 
 echo.
 echo ======================================
 echo FLASH SUCCESSFUL
-if "%TARGET_CHOICE%"=="2" (
-    echo Partitioning done for Slot A
-)
-if "%TARGET_CHOICE%"=="3" (
-    echo Partitioning done for Slot B
-)
 echo ======================================
 goto end
 
